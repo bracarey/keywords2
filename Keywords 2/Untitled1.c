0197a3104f16d9c49c84874ad77556879b6fd2e2
bracarey <44170328+bracarey@users.noreply.github.com>
2019-04-11 07:16:50 -0500

    Add files via upload

---
+++ Keywords 2/Untitled1.c
@@ -0,0 +1,50 @@
+while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
+    {
+        cout << "\n\nYou have " << (MAX_WRONG - wrong);
+		cout << " incorrect guesses left.\n";
+        cout << "\nYou've used the following letters:\n" << used << endl;
+        cout << "\nSo far, the word is:\n" << soFar << endl;
+
+        char guess;
+        cout << "\n\nEnter your guess: ";
+        cin >> guess;
+        guess = toupper(guess); //make uppercase since secret word in uppercase
+        while (used.find(guess) != string::npos)
+        {
+            cout << "\nYou've already guessed " << guess << endl;
+            cout << "Enter your guess: ";
+            cin >> guess;
+            guess = toupper(guess);
+        }
+
+        used += guess;
+
+        if (THE_WORD.find(guess) != string::npos)
+        {
+            cout << "That's right! " << guess << " is in the word.\n";
+
+            // update soFar to include newly guessed letter
+            for (unsigned int i = 0; i < THE_WORD.length(); ++i)
+			{
+                if (THE_WORD[i] == guess)
+				{
+                    soFar[i] = guess;
+				}
+			}
+        }
+        else
+        {
+            cout << "Sorry, " << guess << " isn't in the word.\n";
+            ++wrong;
+        }
+    }
+
+    // shut down
+    if (wrong == MAX_WRONG)
+        cout << "\nYou've been hanged!";
+    else
+        cout << "\nYou guessed it!";
+
+    cout << "\nThe word was " << THE_WORD << endl;
+
+    return 0;
